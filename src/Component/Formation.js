7e3084dcd5a34613066de4144f26f2d8204a72c2
unknown <philippe.adisson.pro@gmail.com>
2025-08-26 14:11:57 +0200

    img

--- src/Component/Formation.js
+++ src/Component/Formation.js
@@ -10,17 +10,17 @@ function Formation({language}) {
     <h1 className='title underline'><MachineEcrire text={translations[language].formationCertification}></MachineEcrire></h1>
     <div className='formation__container'>
       {/* <h3 style={{marginTop:50}}>Certification ISTQB</h3> */}
-      <img style={{marginTop:25}} src="./istqb.png" className="istqb logo" alt="logo" width={150} />
+      <img style={{marginTop:25}} src="./istqb.png" className="istqb logo" alt="logo"/>
     <div className='texte-formation__container'>
       <p className='formation-text'>{translations[language].istqb}<a href='' className='link underline' target="_blank">ISTQB</a></p>     
     </div>
       {/* <h3 style={{marginTop:35}}>{translations[language].opquastCertification}</h3> */}
-    <img style={{marginTop:35}} src={opquast} className="opquast logo" alt="logo" width={150}  />
+    <img style={{marginTop:35}} src={opquast} className="opquast logo" alt="logo" />
     <div className='texte-formation__container'>
       <p className='formation-text'>{translations[language].opquastTextCertification}<a href='https://www.opquast.com/' className='link underline' target="_blank">Opquast</a></p>
     </div>
         {/* <h3 style={{marginTop:35}}>{translations[language].programmeZ}</h3> */}
-      <img style={{marginTop:35}} src="https://www.z-association.fr/wp-content/uploads/2024/02/logo_Z-code-pour-emploi.svg" className="programmeZ logo" alt="logo" width={150}  />
+      <img style={{marginTop:35}} src="https://www.z-association.fr/wp-content/uploads/2024/02/logo_Z-code-pour-emploi.svg" className="programmeZ logo" alt="logo" />
       <div className='programmeZ__container  texte-formation__container'> 
        <div className='formation-text'>
         <p>{translations[language].programmeZText}</p>
